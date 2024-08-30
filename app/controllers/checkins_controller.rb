8774e9104ff9336ee5ce54bb269733c4779290de
Luck <le.phi.thong@moneyforward.vn>
2024-08-30 15:59:27 +0700

    fix

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -6,20 +6,26 @@ class CheckinsController < ApplicationController
   def create
     booked = Booking.find_by(user_id: current_user.id, seat_id: params[:seat_id])
 
+    if booked.nil?
+      redirect_to root_path, alert: 'You have not booked this seat'
+      return 
+    end
+
     if Time.now <= booked.from_date + 10.minutes
       booked.check_in = true
 
       if booked.save
-        redirect_to root_path, notice: 'Check-in successful'
+        seat = Seat.find(params[:seat_id])
+        redirect_to root_path, notice: "Check-in seat #{seat.name} successful"
         return
       else
-        redirect_to root_path, notice: 'Check-in failed'
+        redirect_to root_path, alert: 'Your booking has been cancelled because the check-in link expired.'
         return
       end
     else
       booked.destroy
     end
 
-    redirect_to root_path, notice: 'Check-in link expired. Therefore we canceled your booking.'
+    redirect_to root_path, alert: 'Check-in link expired'
   end
 end
