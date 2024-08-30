606891b6cf891c62c80de4d5c6bd5d44c09ea45e
hoangtuyb96 <hoangtuyb96@gmail.com>
2024-08-30 13:28:03 +0700

    asd

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :seat_id, :from_date, :to_date, :check_in)
+  end
 end
