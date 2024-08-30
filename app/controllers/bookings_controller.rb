ed0f650a9f9166bfed14e696eb0bf70419f5483a
hoangtuyb96 <hoangtuyb96@gmail.com>
2024-08-30 14:48:37 +0700

    One user can book 1 seat

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,11 +8,11 @@ class BookingsController < ApplicationController
     # One user can only book one seat at a time
     # If the user has a booking, redirect to the booking page
     if current_user.bookings.any?
-      redirect_to current_user.bookings.first, notice: 'You can only book one seat at a time.'
+      redirect_to seats_path
     else
       @booking = current_user.bookings.new(booking_params)
       if @booking.save
-      redirect_to @booking, notice: 'Booking was successfully created.'
+        redirect_to @booking, notice: 'Booking was successfully created.'
       else
         render seats_path
       end
--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -1,5 +1,6 @@
 class SeatsController < ApplicationController
   def index
     @seats = Seat.includes(:bookings).all
+    @have_booking = current_user.bookings.any?
   end
 end
